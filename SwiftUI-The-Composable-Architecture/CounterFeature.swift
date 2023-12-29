c2c985b99ecd325762e569f391c55a0f3acabb57
mculha <melih.culha.09@gmail.com>
2023-12-29 23:37:13 +0300

    Integration of a HTTP Request in the Architecture and Get the Response

--- SwiftUI-The-Composable-Architecture/CounterFeature.swift
+++ SwiftUI-The-Composable-Architecture/CounterFeature.swift
@@ -11,12 +11,16 @@ import ComposableArchitecture
 @Reducer
 struct CounterFeature {
     struct State: Equatable {
-        var count = 0
+        var count: Int = 0
+        var isLoading: Bool = false
+        var fact: String?
     }
     
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
     }
     
     var body: some ReducerOf<Self> {
@@ -24,10 +28,25 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
+            case .factResponse(let fact):
+                state.fact = fact
+                state.isLoading = false
+                return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                
+                return .run { [count = state.count] send in
+                    let (data,_) = try await URLSession.shared.data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact ))
+                }
             }
         }
     }
